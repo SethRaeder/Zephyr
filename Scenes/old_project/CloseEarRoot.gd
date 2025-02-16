8d9d4851cef3922248d90039268f041b5eb8bb72
Seth Raeder <104173918+SethRaeder@users.noreply.github.com>
2025-02-15 21:09:46 -0500

    Initial progress of project structure cleanup.

--- Scenes/old_project/CloseEarRoot.gd
+++
@@ -1,5 +0,0 @@
-extends Node2D
-
-func _spreadEar(enable: bool):
-	$CloseEar.visible = !enable
-	$CloseEarSneeze.visible = enable
--- Scenes/old_project/NostrilRoot.gd
+++
@@ -1,5 +0,0 @@
-extends Node2D
-
-func _flare(val:bool):
-	$Nostril.visible = !val
-	$NostrilWide.visible = val
