f51bcb8afa5ce5092af39b12ddc1855e639d7967
Seth Raeder <104173918+SethRaeder@users.noreply.github.com>
2025-02-15 23:43:46 -0500

    Reorganize audio.

--- Scripts/Characters/Voice/audio_loader.gd
+++ Scripts/Characters/Voice/audio_loader.gd
@@ -16,9 +16,34 @@ func _ready() -> void:
 		
 		var loader := AudioLoader.new()
 		
-		for file : String in DirAccess.get_files_at(load_audio_directory):
+		var files_list = get_all_files(load_audio_directory)
+		
+		for file in files_list:
+			print("File : ",file)
 			if file.ends_with(".wav"):
-				var filepath = load_audio_directory + "/" + file
-				var new_stream : AudioStreamWAV = loader.loadfile(filepath)
+				var new_stream : AudioStreamWAV = loader.loadfile(file)
 				new_stream.loop_mode = AudioStreamWAV.LOOP_DISABLED
 				stream.add_stream(-1, new_stream, 1.0)
+
+func get_all_files(path: String, file_ext := "", files := []):
+	var dir = DirAccess.open(path)
+	if dir != null:
+		dir.list_dir_begin()
+
+		var file_name = dir.get_next()
+
+		while file_name != "":
+			if dir.current_is_dir():
+				files = get_all_files(dir.get_current_dir().path_join(file_name), file_ext, files)
+			else:
+				if file_ext and file_name.get_extension() != file_ext:
+					file_name = dir.get_next()
+					continue
+
+				files.append(dir.get_current_dir().path_join(file_name))
+
+			file_name = dir.get_next()
+	else:
+		print("An error occurred when trying to access %s." % path)
+
+	return files
