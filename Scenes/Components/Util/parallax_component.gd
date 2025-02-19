528ddb74f44493f32daa5e66a58502b30122cdac
Seth Raeder <104173918+SethRaeder@users.noreply.github.com>
2025-02-18 19:18:45 -0500

    Rig Cleanup, animation touch up

--- Scenes/Components/Util/parallax_component.gd
+++ Scenes/Components/Util/parallax_component.gd
@@ -4,5 +4,7 @@ class_name ParallaxMouseMove
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	var screenDimensions = Vector2(get_viewport().size)
-	var desired_pos = (get_global_mouse_position() - (screenDimensions/2))/screenDimensions * scroll_scale
+	var mouse_pos = get_global_mouse_position()
+	mouse_pos = mouse_pos.clamp(Vector2.ZERO, screenDimensions)
+	var desired_pos = (mouse_pos + (screenDimensions/2)) / screenDimensions * scroll_scale
 	scroll_offset = lerp(scroll_offset, desired_pos, delta * 2.0)
---
+++ center_background.gd
@@ -0,0 +1,7 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	var screenDimensions = Vector2(get_viewport().size)
+	position = screenDimensions/2
