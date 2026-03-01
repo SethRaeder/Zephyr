22121111681e164b3d608f66b21216f7b598199e
SethRaeder <104173918+SethRaeder@users.noreply.github.com>
2026-03-01 06:13:32 -0500

    WIP adjustments to try fixing character rig in world

--- scenes/characters/Chance/chance_rig.gd
+++ scenes/characters/Chance/chance_rig.gd
@@ -5,9 +5,9 @@ extends Node
 @export_range(-5,5) var tongue_curl : float = 0:
 	set(new):
 		tongue_curl = new
-		tongue_control.curve_drivers[1].y = new
+		if tongue_control: tongue_control.curve_drivers[1].y = new
 
 @export_range(640,2000,1.0) var tongue_length : float = 650:
 	set(new):
 		tongue_length = new
-		tongue_control.path_length = new
+		if tongue_control: tongue_control.path_length = new
